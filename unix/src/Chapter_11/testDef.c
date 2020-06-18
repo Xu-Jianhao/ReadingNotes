bc1749f119795b637e8529d71b3411f4d8d8c09d
Xu-Jianhao <xjh0493@163.com>
2020-06-18 19:30:10 +0800

    11.5 线程终止

---
+++ unix/src/Chapter_11/testDef.c
@@ -0,0 +1,23 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                             *
+ *    Describe :                                                               *
+ *          测试_POSIX_THREADS宏
+ *
+ *    Author   :  Xu-Jianhao                                                   *
+ *                               Biu Biu Biu !!!                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include "apue.h"
+
+int main(void)
+{
+
+#ifdef _POSIX_THREADS
+    printf("YES!!\n");
+#else
+    printf("NO!!!\n");
+#endif
+
+    exit(0);
+}
+
